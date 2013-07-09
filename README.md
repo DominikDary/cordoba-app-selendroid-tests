40a415bbe6832e990d1a4fbd52f3979c92ed6b0c
Dominik Dary <ddary@acm.org>
2013-07-09 12:49:05 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,8 @@
+cordoba-app-selendroid-tests
+============================
+
+Demo project to show that entering text and clicking a button with selendroid works fine for an Apache cordoba app.
+
+Related to the bug:
+- https://github.com/DominikDary/selendroid/issues/70
+- https://groups.google.com/forum/#!topic/appium-discuss/KFDpA2mC6JM
---
+++ src/main/java/de/dary/cordoba/DemoTest.java
@@ -0,0 +1,48 @@
+package de.dary.cordoba;
+
+import io.selendroid.SelendroidCapabilities;
+import io.selendroid.SelendroidConfiguration;
+import io.selendroid.SelendroidDriver;
+import io.selendroid.SelendroidLauncher;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+  private SelendroidLauncher selendroidServer = null;
+
+  @Test
+  public void testShouldBeAbleToEnterTextAndClickAButton() throws Exception {
+    WebDriver driver = startSelendroidServer();
+    driver.switchTo().window("WEBVIEW");
+
+    WebElement inputField = driver.findElement(By.className("test-text-input"));
+    inputField.sendKeys("Hello Selendroid");
+    WebElement button = driver.findElement(By.className("test-button"));
+    button.click();
+  }
+
+  protected WebDriver startSelendroidServer() throws Exception {
+    if (selendroidServer != null) {
+      selendroidServer.stopSelendroid();
+    }
+    SelendroidConfiguration config = new SelendroidConfiguration();
+    config.addSupportedApp("src/main/resources/HybridApp-debug.apk");
+    config.setPort(4445);
+    selendroidServer = new SelendroidLauncher(config);
+    selendroidServer.lauchSelendroid();
+
+    SelendroidCapabilities caps = SelendroidCapabilities.emulator("com.example.hybridapp:1.0");
+    WebDriver driver = new SelendroidDriver("http://localhost:4445/wd/hub", caps);
+    return driver;
+  }
+
+  // @AfterClass
+  public void stopSelendroidServer() {
+    if (selendroidServer != null) {
+      selendroidServer.stopSelendroid();
+    }
+  }
+}
